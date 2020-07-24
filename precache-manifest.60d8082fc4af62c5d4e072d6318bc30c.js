fa752bdc4dae9a1184cb0e780092e294d6f3df03
unknown <mukherjeerivu7@gmail.com>
2020-07-24 21:03:47 +0530

    Updates

---
+++ precache-manifest.60d8082fc4af62c5d4e072d6318bc30c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaebc83fe02d3ffd2a03dbefa8dd80df",
+    "url": "/react-chartjs-demo/index.html"
+  },
+  {
+    "revision": "6110c35e518bd4ed6a6c",
+    "url": "/react-chartjs-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a537f6a8c07c20008ed5",
+    "url": "/react-chartjs-demo/static/js/2.0858d33a.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/react-chartjs-demo/static/js/2.0858d33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6110c35e518bd4ed6a6c",
+    "url": "/react-chartjs-demo/static/js/main.b90b20be.chunk.js"
+  },
+  {
+    "revision": "01cc89aa62a3d9abdcaa",
+    "url": "/react-chartjs-demo/static/js/runtime-main.7b9bc179.js"
+  }
+]);
